94602e56a59b419c79598c50e3a5d45229e76f55
branch171 <craig.branch@virginmedia.com>
2025-10-06 17:51:01 +0100

    update ProcessSimIO with InitializationProblem and TrustRegion using FiniteDiff. We can have dual numbers going into Clapeyron

--- examples/vscode/ProcessSimIOTest_Rev3.jl
+++ examples/vscode/ProcessSimIOTest_Rev3.jl
@@ -1,9 +1,10 @@
-using ModelingToolkit, DifferentialEquations, Clapeyron, NLsolve
+using ModelingToolkit, NonlinearSolve, DifferentialEquations, Clapeyron, NLsolve
 using ModelingToolkit: t_nounits as t, D_nounits as D
 using ModelingToolkit: scalarize
 using Suppressor
 
-fluid = ["carbon dioxide","nitrogen","water"]
+#fluid = ["carbon dioxide","nitrogen","water"]
+fluid = ["carbon dioxide","nitrogen"]
 nfluid = length(fluid)
 maxphases = 3
 FlashpTSize = 1 + 4*maxphases + 3 + maxphases*nfluid
@@ -71,7 +72,7 @@ function FlashpT(model::EoSModel,p,T,z)
     end
     return res
 end
-@register_array_symbolic FlashpT(model::EoSModel,p,T,z::AbstractVector) begin
+@register_array_symbolic FlashpT(model::EoSModel,p::AbstractFloat,T::AbstractFloat,z::AbstractVector) begin
     size = (FlashpTSize,)
     eltype= eltype(z)
 end
@@ -188,13 +189,14 @@ function Flashph(model::EoSModel,T_in,h_in,z_in,p_out)
     T_out = nlsolve(Flash , [T_in])
     return T_out.zero[1]
 end
-@register_symbolic Flashph(model::EoSModel,T_in::Float64,h_in::Float64,z_in::Array{Float64, nfluid},p_out::Float64)
+@register_symbolic Flashph(model::EoSModel,T_in::AbstractFloat,h_in::AbstractFloat,z_in::AbstractVector,p_out::AbstractFloat)
 
 p = 1.4e5
 T = 25.0+273.15
 zdry=[0.9981, 0.0019]
 zwater=0.025
-z=append!(zdry*(1.0-zwater),zwater)
+#z=append!(zdry*(1.0-zwater),zwater)
+z = zdry
 
 FlashResult = FlashpT(model,p,T,z)
 println("FlashTp $(FlashResult)")
@@ -237,7 +239,8 @@ p_in = 90.0e5
 T_in = 35.0+273.15
 zdry=[0.9981, 0.0019]
 zwater=0.00001
-z_in=append!(zdry*(1.0-zwater),zwater)
+#z_in=append!(zdry*(1.0-zwater),zwater)
+z_in = zdry
 
 FlashResult2 = FlashpT(model,p_in,T_in,z_in)
 println("FlashTp2 $(FlashResult2)")
@@ -351,7 +354,7 @@ function PolytropicCompression(model::EoSModel,T_in,p_in,z,πc,η)
     Tpoly = nlsolve(Compressor , [T_in])
     return Tpoly.zero[1]
 end
-@register_symbolic PolytropicCompression(model::EoSModel,T_in::Float64,p_in::Float64,z::Array{Float64, nfluid},πc::Float64,η::Float64)
+@register_symbolic PolytropicCompression(model::EoSModel,T_in::AbstractFloat,p_in::AbstractFloat,z::AbstractVector,πc::AbstractFloat,η::AbstractFloat)
 
 @component function Compressor(;name, ps, Ts, zs, pd, Td)
     @named inport = fluidPort(pg = ps, Tg = Ts, zg = zs)
@@ -361,13 +364,13 @@ end
        T_in(t)
        p_in(t)
        h_in(t)
-       (z_in(t))[1:nfluid]
+       (z_in(t))[1:nfluid], [description  = "Mole fraction vector",  guess = zs]
 
        s_out(t)
        T_out(t)
        p_out(t)
        h_out(t)
-       (z_out(t))[1:nfluid]
+       (z_out(t))[1:nfluid], [description  = "Mole fraction vector",  guess = zs]
 
     end
     para = @parameters begin
@@ -440,6 +443,48 @@ end
 
 end
 
+@component function Valve(;name, ps, Ts, zs, dps, T_outs)
+    @named inport  = fluidPort(pg = ps, Tg = Ts, zg = zs)
+    @named outport = fluidPort(pg = ps-dps, Tg = T_outs, zg = zs)
+    vars = @variables begin
+       p_out(t)
+       T_out(t), [guess = T_outs]
+       v_out(t)
+       h_out(t)
+       s_out(t)
+       (z_out(t))[1:nfluid], [description  = "Mole fraction vector",  guess = zs]
+       (flash(t))[1:FlashpTSize], [guess = FlashpT(model,ps-dps,T_outs,zs)]
+    end
+    para = @parameters begin
+        dp = 0.5e5, [description = "Pressure drop (Pa)"]
+    end
+    eqs = [
+
+        scalarize(z_out .~ inport.z)
+        p_out ~ inport.p - dp
+        scalarize(flash .~ FlashpT(model,p_out,T_out,z_out))
+        v_out ~ Get_Volume_FlashpT(flash)
+        h_out ~ Get_Enthalpy_FlashpT(flash)
+        s_out ~ Get_Entropy_FlashpT(flash)
+        #T_out ~ Flashph(model::EoSModel,T_out,inport.h,z_out,p_out)
+
+        outport.p ~ p_out
+        outport.T ~ T_out
+        outport.v ~ v_out
+        outport.h ~ h_out
+        outport.s ~ s_out
+        scalarize(outport.z .~ z_out)
+        outport.mdot ~ inport.mdot
+        h_out  ~ inport.h
+
+
+    ]
+
+#    eqs = Symbolics.scalarize.(reduce(vcat, Symbolics.scalarize.(eqs)))    
+    compose(ODESystem(eqs, t, collect(Iterators.flatten(vars)), para;name=name),inport,outport)
+
+end
+
 @component function Separator(;name, pg, Tg, zg)
     @named inport = fluidPort(pg = pg, Tg = Tg, zg = zg)
     @named outport1 = fluidPort(pg = pg, Tg = Tg, zg = zg) # on top
@@ -454,7 +499,7 @@ end
        (z_in(t))[1:nfluid]
 
        mdot_out1(t)
-        p_out1(t)
+       p_out1(t)
        T_out1(t)
        v_out1(t)
        h_out1(t)
@@ -530,7 +575,9 @@ p = 1.4e5
 T = 25.0+273.15
 zdry=[0.9981, 0.0019]
 zwater=0.025
-z=append!(zdry*(1.0-zwater),zwater)
+#z=append!(zdry*(1.0-zwater),zwater)
+z = zdry
+
 p1 = p
 T1 = T
 p2 = 4.528*p1
@@ -541,6 +588,13 @@ p4 = 4.528*p3
 T4 = 175.53+273.15
 p5 = p4 - 0.5e5
 T5 = 35.0+273.15
+T6 = 7.0+273.15
+p6 = 88.3e5
+T6 = 150.0+273.15
+p7 = p6 - 0.5e5
+T7 = 35.0+273.15
+p8 = 30.0e5
+T8 = -3.0+273.15
 
 @suppress_err begin
 
@@ -550,18 +604,27 @@ T5 = 35.0+273.15
             heatexchanger1, 
             separator2, 
             compressor2, 
-            heatexchanger2, 
+            heatexchanger2,
+            compressor3, 
+            heatexchanger3,
+            valve,
+            separator3, 
             sink, 
             eqs, systems, flowsheet, sys, u0, para, prob
 
-    @named src = MassSource(ps = p1, Ts = T1, mdots = 50, zs = z)
+    @named src = MassSource(ps = p1, Ts = T1, zs = z, mdots = 50)
     @named separator1 = Separator(pg = p1, Tg = T1, zg = z)
     @named compressor1 = Compressor(ps = p1, Ts = T1, zs = z, pd = p2, Td = T2)
     @named heatexchanger1 = HeatExchanger(ps = p2, Ts = T2, zs = z)
     @named separator2 = Separator(pg = p3, Tg = T3, zg = z)
     @named compressor2 = Compressor(ps = p3, Ts = T3, zs = z, pd = p4, Td = T4)
     @named heatexchanger2 = HeatExchanger(ps = p4, Ts = T4, zs = z)
-    @named sink =  MassSink(ps = p5, Ts = T5, zs = z)
+    @named separator3 = Separator(pg = p5, Tg = T5, zg = z)
+    @named compressor3 = Compressor(ps = p5, Ts = T5, zs = z, pd = p6, Td = T6)
+    @named heatexchanger3 = HeatExchanger(ps = p6, Ts = T6, zs = z)
+    @named valve = Valve(ps = p7, Ts = T7, zs = z, dps = p7 - p8 ,T_outs = T8)
+    @named separator4 = Separator(pg = p8, Tg = T8, zg = z)
+    @named sink =  MassSink(ps = p8, Ts = T8, zs = z)
 
     eqs =   [
             connect(src.port,separator1.inport)
@@ -570,24 +633,47 @@ T5 = 35.0+273.15
             connect(heatexchanger1.outport,separator2.inport)
             connect(separator2.outport1,compressor2.inport)
             connect(compressor2.outport,heatexchanger2.inport)
-            connect(heatexchanger2.outport,sink.port)
+            connect(heatexchanger2.outport,separator3.inport)
+            connect(separator3.outport1,compressor3.inport)
+            connect(compressor3.outport,heatexchanger3.inport)
+            connect(heatexchanger3.outport,valve.inport)
+            connect(valve.outport,separator4.inport)
+            connect(separator4.outport1,sink.port)
             ] # Define connections
 
-    systems=[src,separator1,compressor1,heatexchanger1,separator2,compressor2,heatexchanger2,sink] # Define system
+    systems=[src,separator1,compressor1,heatexchanger1,separator2,compressor2,heatexchanger2,separator3,compressor3,heatexchanger3,valve,separator4,sink] # Define system
 
     @named flowsheet = System(eqs, t, systems=systems)
     sys = structural_simplify(flowsheet)
     u0 = []
+    guess = []
     para =  [   
-                sys.compressor1.πc =>4.528,
-                sys.compressor1.η =>0.84,
-                sys.compressor2.πc =>4.528,
-                sys.compressor2.η =>0.84 
+                sys.compressor1.πc => 4.528,
+                sys.compressor1.η => 0.84,
+                sys.compressor2.πc => 4.528,
+                sys.compressor2.η => 0.84,
+                sys.compressor3.πc => 3.404,
+                sys.compressor3.η => 0.84,
+                sys.valve.dp => p7 - p8 
             ] # boundary conditions and parameters
 
 
-    prob = SteadyStateProblem(sys,u0,para)
-    sol = solve(prob)
+    #prob = SteadyStateProblem(sys,u0,para)
+    #sol = solve(prob)
+
+#    tspan = (0,1)
+#    prob = ODEProblem(sys,u0,tspan,para)
+#    sol = solve(prob,saveat=1.0)
+
+    iprob = ModelingToolkit.InitializationProblem(sys,0.0,u0,para)
+    sol = solve(iprob, TrustRegion(autodiff=AutoFiniteDiff()))
+
+        srcflash = fill(0.0,FlashpTSize)
+        for ic = 1:FlashpTSize
+            srcflash[ic] = sol[src.flash[ic]][1]
+        end
+    
+    println("src flash = $(srcflash)")
 
     println("Compressor 1 discharge temperature = $(round(sol[compressor1.T_out][1]-273.15,digits=2)) deg C")
     comp1_h_in = sol[compressor1.h_in][1]
@@ -596,7 +682,13 @@ T5 = 35.0+273.15
     println("Compressor 1 h_out = $(round(comp1_h_out,digits=2)) J/mol")
     comp1_mdot = sol[compressor1.inport.mdot][1]
     println("Compressor 1 mdot = $(round(comp1_mdot,digits=2)) kg/s")
-    comp1_mw =  Molecular_Weight(model,sol[compressor1.z_out])
+
+        zz = fill(0.0,nfluid)
+        for ic = 1:nfluid
+            zz[ic] = sol[compressor1.z_out[ic]][1]
+        end
+
+    comp1_mw =  Molecular_Weight(model,zz)
     println("Compressor 1 mw = $(round(comp1_mw*1000,digits=2)) kg/kmol")
     power1 = comp1_mdot*(comp1_h_out - comp1_h_in)/comp1_mw/1000.0/1000.0
     println("Compressor 1 power = $(round(power1,digits=2)) MW")
@@ -606,18 +698,34 @@ T5 = 35.0+273.15
     println("Heat Exchanger 1 h_out = $(round(hx1_h_out,digits=2)) J/mol")
     hx1_mdot = sol[heatexchanger1.inport.mdot][1]
     println("Heat Exchanger 1 mdot = $(round(hx1_mdot,digits=2)) kg/s")
-    hx1_mw =  Molecular_Weight(model,sol[heatexchanger1.z_out])
+
+        zz = fill(0.0,nfluid)
+        for ic = 1:nfluid
+            zz[ic] = sol[heatexchanger1.z_out[ic]][1]
+        end
+
+    hx1_mw =  Molecular_Weight(model,zz)
     println("Heat Exchanger 1 mw = $(round(hx1_mw*1000,digits=2)) kg/kmol")
     duty1 = hx1_mdot*(hx1_h_out - hx1_h_in)/hx1_mw/1000.0/1000.0
     println("Heat Exchanger 1 duty = $(round(duty1,digits=2)) MW")
     println("Compressor 2 discharge temperature = $(round(sol[compressor2.T_out][1]-273.15,digits=2)) deg C")
+    comp2_p_in = sol[compressor2.p_in][1]
+    println("Compressor 2 p_in = $(round(comp2_p_in,digits=2)) Pa")
+    comp2_p_out = sol[compressor2.p_out][1]
+    println("Compressor 2 p_out = $(round(comp2_p_out,digits=2)) Pa")
     comp2_h_in = sol[compressor2.h_in][1]
     println("Compressor 2 h_in = $(round(comp2_h_in,digits=2)) J/mol")
     comp2_h_out = sol[compressor2.h_out][1]
     println("Compressor 2 h_out = $(round(comp2_h_out,digits=2)) J/mol")
     comp2_mdot = sol[compressor2.inport.mdot][1]
     println("Compressor 2 mdot = $(round(comp2_mdot,digits=2)) kg/s")
-    comp2_mw =  Molecular_Weight(model,sol[compressor2.z_out])
+
+        zz = fill(0.0,nfluid)
+        for ic = 1:nfluid
+            zz[ic] = sol[compressor2.z_out[ic]][1]
+        end
+
+    comp2_mw =  Molecular_Weight(model,zz)
     println("Compressor 2 mw = $(round(comp2_mw*1000,digits=2)) kg/kmol")
     power2 = comp2_mdot*(comp2_h_out - comp2_h_in)/comp2_mw/1000.0/1000.0
     println("Compressor 2 power = $(round(power2,digits=2)) MW")
@@ -627,9 +735,28 @@ T5 = 35.0+273.15
     println("Heat Exchanger 2 h_out = $(round(hx2_h_out,digits=2)) J/mol")
     hx2_mdot = sol[heatexchanger2.inport.mdot][1]
     println("Heat Exchanger 2 mdot = $(round(hx2_mdot,digits=2)) kg/s")
-    hx2_mw =  Molecular_Weight(model,sol[heatexchanger2.z_out])
+
+        zz = fill(0.0,nfluid)
+        for ic = 1:nfluid
+            zz[ic] = sol[heatexchanger2.z_out][1]
+        end
+
+    hx2_mw =  Molecular_Weight(model,zz)
     println("Heat Exchanger 2 mw = $(round(hx2_mw*1000,digits=2)) kg/kmol")
     duty2 = hx2_mdot*(hx2_h_out - hx2_h_in)/hx2_mw/1000.0/1000.0
     println("Heat Exchanger 2 duty = $(round(duty2,digits=2)) MW")
+    println("Valve outlet temperature = $(round(sol[valve.T_out][1]-273.15,digits=2)) deg C")
+    val1_h_out = sol[valve.outport.h][1]
+    println("Valve h_out = $(round(val1_h_out,digits=2)) J/mol")
+    val1_p_in = sol[valve.inport.p][1]
+    println("Valve p_in = $(round(val1_p_in/1e5,digits=2)) bara")
+    val1_p_out = sol[valve.outport.p][1]
+    println("Valve p_out = $(round(val1_p_out/1e5,digits=2)) bara")
+
+    sep4_vapmdot = sol[separator4.outport1.mdot][1]
+    println("Separator 4 vapour mdot = $(round(sep4_vapmdot,digits=2)) kg/s")
+
+    sep4_liqmdot = sol[separator4.outport2.mdot][1]
+    println("Separator 4 liquid mdot = $(round(sep4_liqmdot,digits=2)) kg/s")
 
 end
